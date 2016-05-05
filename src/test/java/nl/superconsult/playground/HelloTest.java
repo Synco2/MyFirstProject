931232a78a2227b842358900a8e2ac502abccc1c
Synco2 <synco.van.deursen@superconsult.nl>
2016-05-05 23:27:54 +0200

    Update HelloTest.java

--- src/test/java/nl/superconsult/playground/HelloTest.java
+++ src/test/java/nl/superconsult/playground/HelloTest.java
@@ -1,5 +1,13 @@
 package nl.superconsult.playground;
 
+import org.junit.Test;
+
 class HelloTest {
   
+  @Test
+  public final void helloTest() {
+    Hello h = new Hello();
+    
+    assertEquals("Hello", h.sayHello());
+  }
 }
